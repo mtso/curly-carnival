f04af8d3a3500b0d7a9d4085e50db5c2e32f34c7
Matthew S Tso <mtso@business-cs.deanza.ad.fhda.edu>
2016-09-28 13:51:54 -0700

    Initial commit

---
+++ 0926-inclass/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << argc << endl;
+	cout << argv[0] << endl;
+	cout << "Hello world" << endl << endl;
+
+	system("pause");
+	return 0;
+}
